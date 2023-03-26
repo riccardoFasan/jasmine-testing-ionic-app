d5a7f3be703f11ffd8d481581033926a00f5e6e6
richard <fasanriccardo21@gmail.com>
2023-03-26 18:53:17 +0200

    feat: aphorisms container component

--- src/app/pages/aphorisms/containers/aphorisms/aphorisms.component.spec.ts
+++ src/app/pages/aphorisms/containers/aphorisms/aphorisms.component.spec.ts
@@ -1,40 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { AphorismsComponent } from './aphorisms.component';
 import { AphorismsStoreService } from '../../store';
 import { HttpClientModule } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { Aphorism } from '@app/core/models';
-
-const MOCK_INITIAL_APHORISMS: Observable<Aphorism[]> = of([]);
-const MOCK_INITIAL_LOADING: Observable<boolean> = of(false);
-const MOCK_INITIAL_COUNT: Observable<number> = of(0);
-const MOCK_INITIAL_QUERY: Observable<string | undefined> = of(undefined);
-const MOCK_INITIAL_PAGE: Observable<number> = of(1);
-const MOCK_INITIAL_PAGE_SIZE: Observable<number> = of(10);
 
 describe('AphorismsComponent', () => {
   let component: AphorismsComponent;
   let fixture: ComponentFixture<AphorismsComponent>;
+  let store: AphorismsStoreService;
 
   beforeEach(async () => {
-    const storeSpy: jasmine.SpyObj<AphorismsStoreService> =
-      jasmine.createSpyObj('AphorismsStoreService', ['search', 'getPage'], {
-        aphorisms$: MOCK_INITIAL_APHORISMS,
-        loading$: MOCK_INITIAL_LOADING,
-        count$: MOCK_INITIAL_COUNT,
-        query$: MOCK_INITIAL_QUERY,
-        page$: MOCK_INITIAL_PAGE,
-        pageSize$: MOCK_INITIAL_PAGE_SIZE,
-      });
+    const storeServiceSpy: jasmine.SpyObj<AphorismsStoreService> =
+      jasmine.createSpyObj('AphorismsStoreService', ['search', 'getPage']);
 
     await TestBed.configureTestingModule({
       imports: [AphorismsComponent, IonicModule.forRoot(), HttpClientModule],
-      providers: [{ provide: AphorismsStoreService, useValue: storeSpy }],
     }).compileComponents();
 
+    TestBed.overrideProvider(AphorismsStoreService, {
+      useValue: storeServiceSpy,
+    });
+
     fixture = TestBed.createComponent(AphorismsComponent);
+    store = TestBed.inject(AphorismsStoreService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -43,7 +31,15 @@ describe('AphorismsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // should load a page of aphorisms on page change
+  it('should load a page of aphorisms on page change', () => {
+    const MOCK_PAGE: number = 2;
+    component.onPageChange(MOCK_PAGE);
+    expect(store.getPage).toHaveBeenCalled();
+  });
 
-  // should load a page of aphorisms on query change
+  it('should load a page of aphorisms on query change', () => {
+    const MOCK_QUERY: string = 'content';
+    component.onQueryChange(MOCK_QUERY);
+    expect(store.search).toHaveBeenCalledWith(MOCK_QUERY);
+  });
 });
--- src/app/pages/aphorisms/containers/aphorisms/aphorisms.component.ts
+++ src/app/pages/aphorisms/containers/aphorisms/aphorisms.component.ts
@@ -3,6 +3,8 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AphorismsStoreService } from '../../store';
 import { provideComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
+import { Aphorism } from '@app/core/models';
 
 @Component({
   selector: 'app-aphorisms',
@@ -17,23 +19,45 @@ import { provideComponentStore } from '@ngrx/component-store';
       </ion-toolbar>
     </ion-header>
 
-    <ion-content [fullscreen]="true">
+    <ion-content
+      [fullscreen]="true"
+      *ngIf="{
+        aphorisms: aphorisms$ | async,
+        loading: loading$ | async,
+        count: count$ | async,
+        query: query$ | async,
+        page: page$ | async,
+        pageSize: pageSize$ | async
+      } as vm"
+    >
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Aphorisms</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <div id="container"></div>
+      <!-- TODO: search component with queryChange output and query input  -->
+
+      <!-- TODO: cards list with count, page, loading, aphorisms and pageSize inputs and pageChange output  -->
     </ion-content>
   `,
-  styles: [
-    `
-      #container {
-      }
-    `,
-  ],
+  styles: [],
 })
 export class AphorismsComponent {
   private readonly store: AphorismsStoreService = inject(AphorismsStoreService);
+
+  protected readonly aphorisms$: Observable<Aphorism[]> = this.store.aphorisms$;
+  protected readonly loading$: Observable<boolean> = this.store.loading$;
+  protected readonly count$: Observable<number> = this.store.count$;
+  protected readonly query$: Observable<string | undefined> = this.store.query$;
+  protected readonly page$: Observable<number> = this.store.page$;
+  protected readonly pageSize$: Observable<number> = this.store.pageSize$;
+
+  onPageChange(page: number): void {
+    this.store.getPage(page);
+  }
+
+  onQueryChange(query: string): void {
+    this.store.search(query);
+  }
 }
