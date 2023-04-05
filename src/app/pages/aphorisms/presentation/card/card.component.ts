853f3df440239f3b540975767dc91c7ec4a632de
richard <fasanriccardo21@gmail.com>
2023-04-05 14:33:38 +0200

    refactor: removed import

--- src/app/pages/aphorisms/presentation/card/card.component.ts
+++ src/app/pages/aphorisms/presentation/card/card.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  ViewEncapsulation,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { Aphorism } from '@app/core/models';
